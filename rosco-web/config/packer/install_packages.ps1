156ef9decea19584eb296eef969a019314125397
Amanda Baker <t-ambak@microsoft.com>
2016-06-20 11:13:55 -0700

    Add ability to download zip, unzip, and execute install script

--- rosco-web/config/packer/install_packages.ps1
+++ rosco-web/config/packer/install_packages.ps1
@@ -1,8 +1,3 @@
-
-
-
-echo "Hello World"
-
 # Strip the first part to avoid credentials leaks.
 #echo repository=$(echo $repository | %{$_ -replace '[.*@^]+', ''}
 #echo package_type=$package_type
@@ -12,17 +7,25 @@ echo "Hello World"
 # Strip leading/trailing quotes if present.
 #repository = echo $repository | %{$_ - replace '[]+', ''}
 
-# Strip leading/trailing quotes if present.
-# Also convert a comma-separated list to a whitespace-separated one.
-#packages = echo $packages | %{$_ -replace ',' , ' '}
+# Public location of zip file
+echo "repository=$repository"
 
+# Download zip
+$storageDir = $pwd
+$webclient = New-Object System.Net.WebClient
+$url = $repository
+$file = "$storageDir\myapp.zip"
+$webclinet.DownloadFile($url, $file)
 
-#function provision_nuget {
-#
-#    # Download NuGet
-#    $storageDir = $pwd
-#    $webclient = New-Object System.Net.WebClient
-#    $url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-#    $file = "$storageDir\NuGet.Tools.vsix"
-#    $webclinet.DownloadFile($url, $file)
-#}
+# Unzip files
+$shell = new-object -com shell.application
+$destination = $pwd
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items()){
+    $shell.Namespace($destination).copyhere($item)
+}
+
+# If there is a install.ps1 then execute it
+If (Test-Path $storageDir\install.ps1) {
+    powershell -executionpolicy bypass -File .\install.ps1
+}
